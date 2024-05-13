e7750dd89625effe9fa03727a8f6c4db90917b6a
nguyenthiyenvi20130463 <20130463@st.hcmuaf.edu.vn>
2024-05-13 08:17:57 +0700

    update ProductDetails and Cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
+import Cart from './customer/components/Cart/Cart';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigation';
 import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/productdetails';
 import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
@@ -11,7 +13,10 @@ function App() {
       <Navigation/>
       <div>
         {/* <HomePage/>       */}
-        <Product/>
+        {/* <Product/> */}
+       {/* <ProductDetails/> */}
+       <Cart/>
+       {/* <Checkout/> */}
       </div>
       <Footer/>
     </div>
