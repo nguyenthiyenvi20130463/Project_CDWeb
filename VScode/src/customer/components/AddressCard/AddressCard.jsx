61b6266699a9783fab105fbe9d2ff72148613da0
nguyenthiyenvi20130463 <20130463@st.hcmuaf.edu.vn>
2024-06-12 17:40:18 +0700

    update thanh toan

--- VScode/src/customer/components/AddressCard/AddressCard.jsx
+++ VScode/src/customer/components/AddressCard/AddressCard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const AddressCard = ({address}) => {
+const AddressCard = ({ address }) => {
     return (
         <div>
             <div className="space-y-3">
                 <p className="font-semibold">{address?.firstName + " " + address?.lastName}</p>
-                <p>{address?.state}, {address?.city}</p>
+                {/* <p>{address?.state}, {address?.city}</p> */}
+                <p>{address?.state},{address?.streetAddress},{address?.zipCode}</p>
                 <div className="space-y-1">
                     <p className="font-semibold">Số điện thoại</p>
                     <p>{address?.mobile}</p>
---
+++ VScode/src/customer/components/Payment/PaymentSuccess.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { store } from '../../../State/Store';
+import { updatePayment } from '../../../State/Payment/Action';
+import { Alert, AlertTitle, Grid } from '@mui/material';
+import OrderTraker from '../Order/OrderTraker';
+import AddressCard from '../AddressCard/AddressCard';
+import { getOrderById } from '../../../State/Order/Action';
+
+const PaymentSuccess = () => {
+    const [paymentId, setPaymentId] = useState();
+    const [referenceId, setReferenceId] = useState();
+    const [paymentStatus, setPaymentStatus] = useState();
+    const { orderId } = useParams();
+
+    const dispatch = useDispatch();
+    const { order } = useSelector(store => store);
+
+    console.log("order ", order.order)
+    useEffect(() => {
+        const urlParam = new URLSearchParams(window.location.search);
+
+        setPaymentId(urlParam.get("razorpay_payment_id"))
+        setPaymentStatus(urlParam.get("razorpay_payment_link_status"))
+    }, [])
+
+    useEffect(() => {
+        if (paymentId) {
+            const data = { orderId, paymentId }
+            dispatch(getOrderById(orderId));
+            dispatch(updatePayment(data))
+        }
+
+
+    }, [orderId, paymentId])
+
+    return (
+        <div className='px-2 lg:px-36'>
+
+            <div className='flex flex-col justify-center items-center'>
+
+                <Alert
+                    variant='filled'
+                    security='success'
+                    sx={{ mb: 6, width: "fit-content" }}
+                >
+                    <AlertTitle>Payment Success</AlertTitle>
+                    Xin chúc mừng Đơn hàng của bạn đã được đặt
+
+                </Alert>
+            </div>
+
+            <OrderTraker activeStep={1} />
+
+            <Grid container className='space-y-5 py-5 pt-20'>
+
+                {order.order?.orderItems.map((item) => <Grid Grid container item className='shadow-xl rounded-md p-5'
+                    sx={{ alignItems: "center", justifyContent: "space-between" }}
+                >
+                    <Grid item xs={6}>
+
+                        <div className='flex items-center'>
+
+                            <img className='w-[5rem] h-[5rem] object-cover object-top' src={item.product.imageUrl} alt='' />
+
+                            <div className='ml-5 space-y-2'>
+                                <p>{item.product.title}</p>
+                                <div className='opacity-50 text-xs font-semibold space-x-5'>
+                                    <span>Color: {item.color}</span>
+                                    <span>Size: {item.size}</span>
+                                </div>
+                                <p>Seller : {item.product.brand}</p>
+                                <p> {item.price} VND</p>
+
+                            </div>
+                        </div>
+
+                    </Grid>
+                    <Grid item>
+                        <AddressCard address={order.order?.shippingAddress} />
+
+                    </Grid>
+
+                </Grid>)}
+
+            </Grid>
+
+        </div >
+    )
+}
+
+export default PaymentSuccess
